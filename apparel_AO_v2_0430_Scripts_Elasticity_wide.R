3d1b780be9a17d59d943ffe1951776761a868ec3
sjudarn88 <36339685+sjudarn88@users.noreply.github.com>
2018-11-12 10:14:52 -0800

    Update apparel_AO_v2_0430_Scripts_Elasticity_wide.R

--- apparel_AO_v2_0430_Scripts_Elasticity_wide.R
+++ apparel_AO_v2_0430_Scripts_Elasticity_wide.R
@@ -5,7 +5,10 @@ div_str <- args[7]
 div <- as.numeric(strsplit(args[7], "_")[[1]])
 soar <- args[8]
 season <- args[9]
+
+#create data frame
 dt <- fread(paste0("./", store, "_", soar, "_", div_str,"_",season, "/elasticity_data_store_", store, "_", soar,"_", div_str,"_",season, ".csv"))
+#keep certain columns
 dt <- dt[div_no %in% div, .(div_ln_cls, wk_nbr, log_UNITS, log_price)]
 temp <- copy(dt)
 temp_w <- dcast(temp, formula = wk_nbr ~ div_ln_cls, value.var = "log_price")
