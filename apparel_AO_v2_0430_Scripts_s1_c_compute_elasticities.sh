9e2c1e051bf8e435d8c72994c7a291bad3616217
sjudarn88 <36339685+sjudarn88@users.noreply.github.com>
2019-01-02 19:49:11 -0800

    Update apparel_AO_v2_0430_Scripts_s1_c_compute_elasticities.sh

--- apparel_AO_v2_0430_Scripts_s1_c_compute_elasticities.sh
+++ apparel_AO_v2_0430_Scripts_s1_c_compute_elasticities.sh
@@ -4,6 +4,6 @@ store=$1
 div=$2
 soar=$3
 season=$4
-/home/peddakota_vikash/anaconda3/bin/python Code_Elasticities_new.py $store $div $soar $season
+/home/dan_shao/anaconda3/bin/python Code_Elasticities_new.py $store $div $soar $season
 
 exit 0
